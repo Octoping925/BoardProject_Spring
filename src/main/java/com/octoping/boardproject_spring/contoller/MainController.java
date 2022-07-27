e1535bbd5aa02ddd5f480274e96e4661993de246
Octoping <myc1365@naver.com>
2022-07-28 00:42:59 +0900

    FEAT: 로그인 및 회원가입 기능 추가

--- src/main/java/com/octoping/boardproject_spring/contoller/MainController.java
+++ src/main/java/com/octoping/boardproject_spring/contoller/MainController.java
@@ -4,6 +4,7 @@ import com.octoping.boardproject_spring.domain.Member;
 import com.octoping.boardproject_spring.service.MainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,14 +16,32 @@ public class MainController {
 
     @GetMapping("/")
     public String login() {
+        // TODO: 세션 존재할 시 자동 로그인 기능 추가
         return "login";
     }
 
     @PostMapping("/login")
-    public String getLogin(@RequestParam("id") String id, @RequestParam("password") String password) {
-        Member member = mainService.login(id, password);
-        System.out.println(id + " " + password + " " + member);
+    public String getLogin(@RequestParam("id") String id, @RequestParam("password") String password, Model model) {
+        boolean loginResult = mainService.login(id, password);
+        System.out.println(id + " " + password + " " + loginResult);
+
+        if(!loginResult) {
+            model.addAttribute("message", "로그인에 실패하였습니다"); // 로그인 상태 메시지
+            return "login";
+        }
+
         return "index";
+    }
+
+    @GetMapping("/signUp")
+    public String signUp() {
+        return "signUp";
+    }
+
+    @PostMapping("/signUp")
+    public String joinNewMember(@RequestParam("id") String id, @RequestParam("password") String password, Model model) {
+        boolean result = mainService.joinNewMember(id, password);
+        return "login";
     }
 
 }
--- src/main/java/com/octoping/boardproject_spring/service/MainService.java
+++ src/main/java/com/octoping/boardproject_spring/service/MainService.java
@@ -10,17 +10,26 @@ import org.springframework.stereotype.Service;
 public class MainService {
     MemberRepository memberRepository = new MemoryMemberRepository();
 
-    public Member login(String id, String password) {
+    public boolean login(String id, String password) {
         Optional<Member> member = memberRepository.findById(id);
         if(member.isEmpty()) { // 아이디가 없을 경우
-            return null;
+            return false;
         }
 
         if(member.filter(m -> password.equals(m.getPassword())).isEmpty()) { // 비밀번호가 틀렸을 경우
-            return null;
+            return false;
         }
 
-        return member.get();
+        return true;
+    }
+
+    public boolean joinNewMember(String id, String password) {
+        if(memberRepository.findById(id).isPresent()) {
+            return false;
+        }
+
+        memberRepository.save(new Member(id, password));
+        return true;
     }
 
 }
